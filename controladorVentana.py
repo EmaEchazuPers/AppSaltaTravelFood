44342717c4f32c9c9f4d3cf3878c5ce143b66376
EmaEchazuPers <ema.echazu2@gmail.com>
2023-08-07 12:51:24 -0300

    Cambio controlador

--- controladorVentana.py
+++ controladorVentana.py
@@ -9,15 +9,17 @@ from vistas import vistaReviews as Vr
 #import de modelos
 from modelos import modeloDestino as Mds
 from modelos import modeloActividad as Mac
+from modelos import modeloUbicacion as Mub
 
 
 class controladorVentana(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title('Salta Travel Food')
-        self.geometry('600x400')
+        #self.geometry('600x400')
 
-        #Es un contenedor de las vistas
+        #Se crea un contenedor de las vistas
+
         container = tk.Frame(self)
         container.pack(side='top',fill='both',expand=True)
         container.grid_columnconfigure(0,weight=1)
@@ -27,6 +29,7 @@ class controladorVentana(tk.Tk):
         self.frames = {}
 
         #Carga las distintas vistas
+
         for f in (Vp.VistaPrincipal,Vds.VistaDestinos,Vb.VistaBusqueda,Vdt.VistaDetalles,
                 Vm.VistaMapa,Vr.VistaReviews):
             frame = f(container, self)
@@ -36,12 +39,15 @@ class controladorVentana(tk.Tk):
         
     
     #Muestra la vista que tenga el container
+
     def mostrar_frame(self, container):
         frame = self. frames[container]
         frame.tkraise()
 
     def cerrar_ventana(self):
         self.destroy()
+
+    #Devuelve lista de objetos
 
     def obtener_destinos(self):
         destinos = Mds.ModeloDestino.carga_json('data\destinos.json')
@@ -50,6 +56,15 @@ class controladorVentana(tk.Tk):
     def obtener_actividaes(self):
         actividades = Mac.ModeloActividad.carga_json('data\\actividades.json')
         return actividades
-
-    #def seleccionar_item(self, event):
-    #    indice = Vdt.VistaDetalles.listbox_destinos.curselection()
+    
+    def obtener_ubicaciones(self):
+        ubicaciones = Mub.ModeloUbicacion.carga_json('data\\ubicaciones.json')
+        return ubicaciones
+    
+    def devolver_ruta_imagen(self,id_destino):
+        destinos = self.obtener_destinos()
+        ruta_imagen = ''
+        for des in destinos:
+            if des.id_destino == id_destino:
+                ruta_imagen = des.imagen
+        return ruta_imagen
